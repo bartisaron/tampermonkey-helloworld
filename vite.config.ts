7eaef8c46323f7800782abe137a3fe19803a0ba9
Bartis Áron <bartisaron@gmail.com>
2026-04-06 00:15:12 +0200

    renaming directory

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,15 @@ export default defineConfig({
       entry: "src/vanenet-hello.ts",
       userscript: {
         name: "Vanenet Hello World",
-        namespace: "https://github.com/bartisaron/userscripts",
+        namespace: "https://github.com/bartisaron/tampermonkey-helloworld",
         version: "0.1.0",
         description: "Hello World on vanenet.hu",
         author: "You",
         match: ["https://vanenet.hu/*", "https://www.vanenet.hu/*"],
         updateURL:
-          "https://bartisaron.github.io/userscripts/vanenet-hello.user.js",
+          "https://bartisaron.github.io/tampermonkey-helloworld/vanenet-hello.user.js",
         downloadURL:
-          "https://bartisaron.github.io/userscripts/vanenet-hello.user.js",
+          "https://bartisaron.github.io/tampermonkey-helloworld/vanenet-hello.user.js",
         grant: "none",
       },
     }),
